87f8e2345ec0d0dc37497b7449cf6a9559028228
IgorOgievich <igorboog@mail.ru>
2020-05-11 18:16:31 +0300

    Updates

---
+++ precache-manifest.2d204378f08f9d4570411f9ce9e48086.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34390f5907ccceec7037986b993d395b",
+    "url": "/portfolio-IgorOgievich/index.html"
+  },
+  {
+    "revision": "ee21af07c713c2ffea8b",
+    "url": "/portfolio-IgorOgievich/static/css/main.fef7c161.chunk.css"
+  },
+  {
+    "revision": "4e23e65c95e0dc6e3fef",
+    "url": "/portfolio-IgorOgievich/static/js/2.8cc49ab1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-IgorOgievich/static/js/2.8cc49ab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee21af07c713c2ffea8b",
+    "url": "/portfolio-IgorOgievich/static/js/main.eeb29d15.chunk.js"
+  },
+  {
+    "revision": "051c7e63367b8fd640fd",
+    "url": "/portfolio-IgorOgievich/static/js/runtime-main.c3329d79.js"
+  },
+  {
+    "revision": "666f5042b8212d7259151863da48f4ad",
+    "url": "/portfolio-IgorOgievich/static/media/PikPng.com_google-play-store-logo_2690057.666f5042.png"
+  },
+  {
+    "revision": "de9fde9c37e349f1625a89ece0d21da0",
+    "url": "/portfolio-IgorOgievich/static/media/code.de9fde9c.png"
+  },
+  {
+    "revision": "763e7a44bc07e8b5f0a97d6e78f2812e",
+    "url": "/portfolio-IgorOgievich/static/media/css.763e7a44.png"
+  },
+  {
+    "revision": "200136878cbcc3d6ded9e21dbb394f84",
+    "url": "/portfolio-IgorOgievich/static/media/git.20013687.png"
+  },
+  {
+    "revision": "baaafe9de4b833c7792807ef2dc083ee",
+    "url": "/portfolio-IgorOgievich/static/media/instagram (1).baaafe9d.png"
+  },
+  {
+    "revision": "07c9dbe6c76fa2dc115ac1f62051774d",
+    "url": "/portfolio-IgorOgievich/static/media/language.07c9dbe6.png"
+  },
+  {
+    "revision": "4df15128efb6773ba7b5c23316430068",
+    "url": "/portfolio-IgorOgievich/static/media/linkedin.4df15128.png"
+  },
+  {
+    "revision": "cba272a52912911eb2a0d7c9b8877e6d",
+    "url": "/portfolio-IgorOgievich/static/media/telegram.cba272a5.png"
+  },
+  {
+    "revision": "b2ec49420025e19430da9c153c0a59ce",
+    "url": "/portfolio-IgorOgievich/static/media/vk.b2ec4942.png"
+  }
+]);
